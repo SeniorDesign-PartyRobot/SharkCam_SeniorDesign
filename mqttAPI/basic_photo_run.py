f4a7772055b3f67be07b6d2e600b27a8e312e8b3
bsenders <bsenders@bu.edu>
2023-02-16 18:34:53 -0500

    added function to force cleaning

--- mqttAPI/basic_photo_run.py
+++ mqttAPI/basic_photo_run.py
@@ -11,19 +11,24 @@ capture_number = 2 # Number of times robot pauses to capture
 capture_interval = 10 # Time between captures
 
 def pause_robot():
-    while mqtt_client.verify_robot_state(4) == False:
+    while mqtt_client.is_paused == False:
         mqtt_client.pause()
         time.sleep(1)
 
 def dock_robot():
-    while mqtt_client.verify_robot_state(7) == False:
+    while mqtt_client.is_docking == False:
         mqtt_client.dock()
+        time.sleep(1)
+
+def clean_NO_VAC():
+    while mqtt_client.is_cleaning() == False:
+        mqtt_client.set_fan_speed(0)
+        mqtt_client.clean
         time.sleep(1)
 
 def move_robot_off_dock_NO_VAC():
      if mqtt_client.is_docked():
-        mqtt_client.set_fan_speed(0)
-        mqtt_client.clean()
+        clean_NO_VAC()
         time.sleep(15)
         pause_robot()
 
@@ -32,13 +37,13 @@ def basic_photo_run(capture_number, capture_interval):
     capture_time = 5 # Amount of time robot pauses to capture
     
     move_robot_off_dock_NO_VAC()
-    mqtt_client.clean()
+    clean_NO_VAC()
     
     for i in range(capture_number):
         time.sleep(capture_interval)
         pause_robot()
         time.sleep(capture_time)
-        mqtt_client.clean()
+        clean_NO_VAC()
 
 basic_photo_run(capture_number,capture_interval)
 dock_robot()

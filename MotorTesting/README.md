0a6eefcae24c4efa002974c0b018b7af5b182d46
AHP-23 <alexchp@bu.edu>
2023-02-23 14:25:16 -0500

    Updated motor testing

--- MotorTesting/README.md
+++ MotorTesting/README.md
@@ -3,9 +3,9 @@ This is the testing of the barebones prototype without networking
 On succesful run of [test_motor.py](test_motor.py), expect the motor to turn
 TODO list:
 - Connect HAT (Done)
-- Make motor turn without the independant power 
+- Make motor turn without the independant power (Done)
 - add magnetic connector set
-- bring USB A male to USB C female 
+- bring USB A male to USB C female (Done)
 - Make motor turn without with the independant power 
 - add Tof Sensor to circuit
 - feature that Tof measurements trigger a flag when object is close
--- MotorTesting/test_motor.py
+++ MotorTesting/test_motor.py
@@ -2,34 +2,30 @@ import RPi.GPIO as GPIO
 import time
 
 # Define the GPIO pins for the motor
-coil_A_1_pin = 11
-coil_A_2_pin = 13
-coil_B_1_pin = 15
-coil_B_2_pin = 16
+print("Hello")
+ControlPin = [11, 13, 15, 16]
 
 # Set up the GPIO pins
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(coil_A_1_pin, GPIO.OUT)
-GPIO.setup(coil_A_2_pin, GPIO.OUT)
-GPIO.setup(coil_B_1_pin, GPIO.OUT)
-GPIO.setup(coil_B_2_pin, GPIO.OUT)
+GPIO.setmode(GPIO.BOARD)
+for pin in ControlPin:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin,0)
 
 # Define the step sequence for the motor
 # sequence set, change comment for different step options
-step_sequence = [[1,0,0,1],[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1]]
+step_sequence = [[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1]]
 
 # Define the number of steps and delay between steps
 num_steps = 512
-delay = 0.005
+delay = 0.001
 
 # Run the motor
 for i in range(num_steps):
     for step in range(8):
-        GPIO.output(coil_A_1_pin, step_sequence[step][0])
-        GPIO.output(coil_A_2_pin, step_sequence[step][1])
-        GPIO.output(coil_B_1_pin, step_sequence[step][2])
-        GPIO.output(coil_B_2_pin, step_sequence[step][3])
+        for pin in range(4):
+            GPIO.output(ControlPin[pin], step_sequence[step][pin])
         time.sleep(delay)
 
 # Clean up the GPIO pins
 GPIO.cleanup()
+print("Goodbye")

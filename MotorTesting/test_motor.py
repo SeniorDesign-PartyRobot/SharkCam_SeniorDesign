589ba85f213034a93d50ed96a982efd1ec2fcc6a
AHP-23 <alexchp@bu.edu>
2023-02-28 14:02:12 -0500

    final version of tests

--- MotorTesting/test_motor.py
+++ MotorTesting/test_motor.py
@@ -1,31 +1,35 @@
+#Simple demo of the stepper motor, does a full rotation
 import RPi.GPIO as GPIO
 import time
 
-# Define the GPIO pins for the motor
-print("Hello")
-ControlPin = [11, 13, 15, 16]
+def run_motor():
+    print("Hello")
 
-# Set up the GPIO pins
-GPIO.setmode(GPIO.BOARD)
-for pin in ControlPin:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin,0)
+    # Set constants
+    ControlPin = [11, 13, 15, 16] # Set the GPIO pins for the motor
+    num_steps = 256 # Set the number of step
+    delay = 0.001 # Set delay between steps
+    step_sequence = [[1,0,0,0],[1,1,0,0], # Set the step sequence for the motor
+                    [0,1,0,0],[0,1,1,0],
+                    [0,0,1,0],[0,0,1,1],
+                    [0,0,0,1],[1,0,0,1]] 
 
-# Define the step sequence for the motor
-# sequence set, change comment for different step options
-step_sequence = [[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1]]
+    # Set up the GPIO pins
+    GPIO.setmode(GPIO.BOARD)
+    for pin in ControlPin:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin,0)
 
-# Define the number of steps and delay between steps
-num_steps = 512
-delay = 0.001
+    # Run the motor
+    for i in range(num_steps):
+        for step in range(8):
+            for pin in range(4):
+                GPIO.output(ControlPin[pin], step_sequence[step][pin])
+            time.sleep(delay)
 
-# Run the motor
-for i in range(num_steps):
-    for step in range(8):
-        for pin in range(4):
-            GPIO.output(ControlPin[pin], step_sequence[step][pin])
-        time.sleep(delay)
+    # Clean up the GPIO pins 
+    GPIO.cleanup()
+    print("Goodbye")
 
-# Clean up the GPIO pins 
-GPIO.cleanup()
-print("Goodbye")
+if __name__ == "__main__":
+    run_motor()
--- MotorTesting/test_sensor.py
+++ MotorTesting/test_sensor.py
@@ -6,30 +6,35 @@
 import board
 import adafruit_vl53l4cd
 
-# set constants
-min_distance = 5
-timing_budget = 200
-inter_measurement = 0
+def run_sensor():
+    # set constants
+    min_distance = 5 # in cm
+    timing_budget = 50
+    inter_measurement = 0
 
-# start script
-print("Hello")
-print("--------------------")
-flag = 0
+    # start script
+    print("Hello")
+    print("--------------------")
+    flag = 0
 
-i2c = board.I2C()  # uses board.SCL and board.SDA
-ToF = adafruit_vl53l4cd.VL53L4CD(i2c)
-ToF.inter_measurement = inter_measurement
-ToF.timing_budget = timing_budget
+    i2c = board.I2C()  # uses board.SCL and board.SDA
+    ToF = adafruit_vl53l4cd.VL53L4CD(i2c)
+    ToF.inter_measurement = inter_measurement
+    ToF.timing_budget = timing_budget
 
-ToF.start_ranging()  # start measurments
-for i in range(10):
-    while not ToF.data_ready:
-        pass
-    ToF.clear_interrupt()
-    print("Distance: {} cm".format(ToF.distance))
-    if ToF.distance <= min_distance and ToF.distance != 0.0:
-        flag = 1
-        print("too close")
+    ToF.start_ranging()  # start measurments
+    for i in range(100):
+        while not ToF.data_ready:
+            pass
+        ToF.clear_interrupt()
+        print("Distance: {} cm".format(ToF.distance))
+        if ToF.distance <= min_distance and ToF.distance != 0.0:
+            flag = 1
+            print("too close")
 
-print("--------------------")
-print("Goodbye")
+    print("--------------------")
+    print("Goodbye")
+    return 1
+
+if __name__ == "__main__":
+    run_sensor

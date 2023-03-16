d7d188cd72a3122585692cd154c459dc2686d250
sgdelia <sgdelia31@gmail.com>
2023-03-16 17:51:10 -0400

    Screen navigation code example

--- photo-app/App.js
+++ photo-app/App.js
@@ -3,6 +3,54 @@
 // base tutorial from here: https://medium.com/@nutanbhogendrasharma/implement-camera-in-react-native-mobile-app-part-1-ea307ce924a4
 // https://www.freecodecamp.org/news/how-to-create-a-camera-app-with-expo-and-react-native/
 
+//////////////////////////////////Test Code/////////////////////////////////////////
+// Navigate between screens: https://reactnavigation.org/docs/navigating
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from 'react-native';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+/////////////////////////////////OLD CODE///////////////////////////////////////////
+/*
 import { CameraType } from 'expo-camera';
 import { Camera } from 'expo-camera';
 import { useState } from 'react';
@@ -82,4 +130,4 @@ const styles = StyleSheet.create({
 });
 
 
-/////////////////////////////////OLD CODE///////////////////////////////////////////
+*/

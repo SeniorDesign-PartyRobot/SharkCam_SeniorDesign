70fce5ee22c7ddb20646e71ad5b93dcdab8d5dda
sgdelia <sgdelia31@gmail.com>
2023-03-19 21:51:16 -0400

    Screen cleanup, camera screen added

--- photo-app/App.js
+++ photo-app/App.js
@@ -11,13 +11,16 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button } from 'react-native';
-import useWebSocket, { ReadyState } from 'react-use-websocket';
-//import TcpSocket from 'react-native-tcp-socket';
+import { Button, Switch, Alert, StyleSheet, TouchableOpacity } from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list';
+import { useState } from 'react';
+import { CameraType } from 'expo-camera';
+import { Camera } from 'expo-camera';
+import { Image } from 'react-native'
 
 
 
-
+/////////////////// Home Screen ////////////////////////
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'top' }}>
@@ -26,17 +29,35 @@ function HomeScreen({ navigation }) {
         title="Robot Controls"
         onPress={() => navigation.navigate('RobotControls')}
       />
+      <Button
+        title="Camera"
+        onPress={() => navigation.navigate('CameraScreen')}
+      />
     </View>
   );
 }
-
+////////////////// Robot Controls /////////////////////////////
 function sendMsg() {
   console.log("button clicked!");
-  ws.send('yeehaw');
-
+  ws.send('hello!');
 };
 
 function RobotControls() {
+  //////////// camera delay code //////////////
+  var delay = 10; // default delay, even if not shown on drop down
+  const [selected, setSelected] = React.useState("");
+  const data = [
+    { key: '1', value: '10 Seconds' },
+    { key: '2', value: '15 Seconds' },
+    { key: '3', value: '30 Seconds' },
+  ]
+  delay = parseInt(selected); // selected from dropdown menu
+  /////////////// auto capture code ///////////////
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => {
+    setIsEnabled(previousState => !previousState);
+  };
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Robot Controls</Text>
@@ -44,21 +65,103 @@ function RobotControls() {
         title="Say hello!"
         onPress={() => sendMsg()}
       />
+      <Text> Auto Capture </Text>
+      <Switch
+        trackColor={{ false: '#767577', true: '#81b0ff' }}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        onValueChange={toggleSwitch}
+        value={isEnabled} />
+      <Text> Delay </Text>
+      <SelectList
+        setSelected={(delay) => setSelected(delay)}
+        data={data}
+        save="value"
+      />
     </View>
   );
 };
 
+///////////////// Camera Screen /////////////////////////
+function CameraScreen() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [cameraRef, setCameraRef] = useState(null);
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type} ref={(ref) => { setCameraRef(ref); }}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>flip</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={async () => {
+            if (cameraRef) {
+              var photo = await cameraRef.takePictureAsync();
+              console.log(photo.uri);
+            }
+          }}>
+            <Text style={styles.text}>take photo</Text>
+
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
+
+
+///////////////// Main //////////////////////////
 // inits are here because I'm tired
 const Stack = createNativeStackNavigator();
 const WS_URL = 'ws://192.168.8.207:8080';
 const ws = new WebSocket(WS_URL);
 
 function App() {
-  const [serverState, setServerState] = React.useState('Loading...');
-  const [messageText, setMessageText] = React.useState('');
-  const [disableButton, setDisableButton] = React.useState(true);
-  const [inputFieldEmpty, setInputFieldEmpty] = React.useState(true);
-  const [serverMessages, setServerMessages] = React.useState([]);
 
   ws.onopen = () => {
     console.log('connected');
@@ -68,12 +171,12 @@ function App() {
     console.log(data);
   }
 
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="RobotControls" component={RobotControls} />
+        <Stack.Screen name="CameraScreen" component={CameraScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );

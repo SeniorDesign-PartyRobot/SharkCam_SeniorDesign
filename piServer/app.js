80c2867113c6e49bd20b6e614ee285f7fbd7348f
AHP-23 <alexchp@bu.edu>
2023-04-04 14:18:05 -0400

    Update app.js

--- piServer/app.js
+++ piServer/app.js
@@ -5,7 +5,21 @@ const WebSocket = require("ws");
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const port = 3000;
+
+//const { spawn } = require('child_process');
+
 const { spawn } = require('child_process');
+const child = spawn('ls',);
+// use child.stdout.setEncoding('utf8'); if you want text chunks
+child.stdout.on('data', (chunk) => {
+    // data from the standard output is here as buffers
+});
+// since these are streams, you can pipe them elsewhere
+child.stderr.pipe(dest);
+child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
+
 
 function startPythonProcess() {
     const process = spawn('python', ['piCommunication.py']);

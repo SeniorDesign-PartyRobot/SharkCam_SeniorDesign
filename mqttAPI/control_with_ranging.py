72a2a2fad3997b7bee539d87004fc3012fc91c21
bsenders <bsenders@bu.edu>
2023-04-03 22:36:59 -0400

    added motor control to main script

--- mqttAPI/control_with_ranging.py
+++ mqttAPI/control_with_ranging.py
@@ -1,6 +1,7 @@
 import time
 import multiprocessing
 import psutil
+import RPi.GPIO as GPIO
 import board
 import adafruit_vl53l4cd
 from common.python_mqtt.mqtt_client import MQTTClient
@@ -17,7 +18,35 @@ ToF.timing_budget = timing_budget
 
 detection = multiprocessing.Event()
 is_calibratedEvent = multiprocessing.Event()
-captureComplete = multiprocessing.Event()
+rotation_complete = multiprocessing.Event()
+capture_Complete = multiprocessing.Event()
+
+
+def run_motor():
+    """Rotates motor four quarter turns"""
+    ControlPin = [11, 13, 15, 16]
+    num_steps = 512 # Set the number of steps for a full rotation
+    delay = 0.001 # Set delay between steps
+    step_sequence = [[1,0,0,0],[1,1,0,0],
+                    [0,1,0,0],[0,1,1,0],
+                    [0,0,1,0],[0,0,1,1],
+                    [0,0,0,1],[1,0,0,1]] 
+
+    # Set up the GPIO pins
+    GPIO.setmode(GPIO.BOARD)
+    for pin in ControlPin:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin,0)
+
+    # Run the motor
+    for i in range(num_steps):
+        for step in range(8):
+            for pin in range(4):
+                GPIO.output(ControlPin[pin], step_sequence[step][pin])
+            time.sleep(delay)
+        if i % 128 == 0: # quarter turn has been completed
+            time.sleep(1.5)
+    rotation_complete.set()   
 
 def ranging():
     print("ranging")
@@ -78,7 +107,7 @@ def basic_photo_run(capture_number: int, capture_interval: int):
     global mqtt_client
     mqtt_client = MQTTClient(robot_ip)
     
-    capture_time = 5 # Amount of time robot pauses to capture
+    #capture_time = 5 # Amount of time robot pauses to capture
     
     move_robot_off_dock_NO_VAC()
     while not is_calibratedEvent.is_set():
@@ -88,9 +117,13 @@ def basic_photo_run(capture_number: int, capture_interval: int):
     for i in range(capture_number):
         time.sleep(capture_interval)
         pause_robot()
-        time.sleep(capture_time)
+        run_motor()
+        while not rotation_complete.is_set():
+            pass
+        #time.sleep(capture_time)
+        rotation_complete.clear()
         mqtt_client.resume()
-    captureComplete.set()
+    capture_Complete.set()
     dock_robot()
 
 
@@ -101,7 +134,7 @@ if __name__ == "__main__":
     captureProcess.start()
     captureProcessPID = captureProcess.pid
 
-    while not captureComplete.set():
+    while not capture_Complete.set():
         while not detection.is_set():
             pass
         psutil.Process(pid=captureProcessPID).suspend()

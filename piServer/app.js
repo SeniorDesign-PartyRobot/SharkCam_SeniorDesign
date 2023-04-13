70e11d94553a01fbf46b405e3b063b11353f4fe0
AHP-23 <alexchp@bu.edu>
2023-04-12 23:23:16 -0400

    Update app.js

--- piServer/app.js
+++ piServer/app.js
@@ -8,38 +8,34 @@ const port = 3000;
 
 //const { spawn } = require('child_process');
 
-const { spawn } = require('child_process');
-const child = spawn('python', ['piCommunication.py']);
-
-// use child.stdout.setEncoding('utf8'); if you want text chunks
-child.stdout.on('data', (chunk) => {
-    // data from the standard output is here as buffers
-    console.log(chunk.toString());
-});
-
-// log any errors that occur
-child.stderr.on('data', (data) => {
-    console.error(`child process stderr:\n${data}`);
-});
-
-child.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-});
-
-
-/*
-
 
 function startPythonProcess() {
-    const process = spawn('python', ['piCommunication.py']);
-    process.stdout.on('data', function (data) {
-        console.log("Data from python script");
-        const data1 = data.toString();
-        console.log(data1);
+    const { spawn } = require('child_process'); // Move this line here
+    const child = spawn('python', ['piCommunication.py']);
+
+    // use child.stdout.setEncoding('utf8'); if you want text chunks
+    child.stdout.on('data', (chunk) => {
+        // data from the standard output is here as buffers
+        console.log(chunk.toString());
     });
-    return process;
+
+    // log any errors that occur
+    child.stderr.on('data', (data) => {
+        console.error(`child process stderr:\n${data}`);
+    });
+
+    child.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+
+    return child; // Return the child process object instead of the undefined "process" variable
 }
 
+
+//Main
+startPythonProcess();
+
+/*
 let currentProcess = null;
 
 wss.on("connection", function connection(ws) {
@@ -71,8 +67,6 @@ wss.on("connection", function connection(ws) {
 });
 
 
-server.listen(8080, () => {
-    console.log("Listening to port 8080");
-});
+
 */
 
